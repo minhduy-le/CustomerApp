9586207d4b811ddea1ba56c3a855a9364a628509
Le Minh Duy <duy.leminh.se@gmail.com>
2025-03-08 09:20:10 +0700

    Calender

--- src/app/(employee)/index.tsx
+++ src/app/(employee)/index.tsx
@@ -1,7 +1,61 @@
-import EmployeeHeader from "@/components/employee/topheader.employee";
+import React, { useState } from "react";
 import { SafeAreaView, View } from "react-native";
+import { Calendar } from "react-native-calendars";
+import EmployeeHeader from "@/components/employee/topheader.employee";
 
 const HomePage = () => {
+  const today = new Date();
+  const currentDate = today.toISOString().split("T")[0]; // Get today's date
+
+  const test = ["2025-03-19", "2025-03-21"]; // Task dates array
+
+  type taskDate = {
+    marked: boolean;
+    selectedTextColor: string;
+  };
+
+  const [markedDates, setMarkedDates] = useState<{
+    [key: string]: {
+      selected?: boolean;
+      selectedColor?: string;
+      selectedTextColor?: string;
+    };
+  }>({});
+
+  const initializeMarkedDates = () => {
+    const initialMarkedDates = test.reduce<{ [key: string]: taskDate }>(
+      (acc, date) => {
+        acc[date] = {
+          marked: true,
+          selectedTextColor: "white",
+        };
+        return acc;
+      },
+      {}
+    );
+    return initialMarkedDates;
+  };
+
+  const markedSelectDate = (day: any) => {
+    const selectedDay = day.dateString;
+
+    const newMarkedDates = {
+      [selectedDay]: {
+        selected: true,
+        selectedColor: "orange",
+        selectedTextColor: "white",
+      },
+      ...initializeMarkedDates(),
+    };
+
+    setMarkedDates(newMarkedDates);
+  };
+
+  const combinedMarkedDates = {
+    ...initializeMarkedDates(),
+    ...markedDates,
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View>
@@ -11,7 +65,13 @@ const HomePage = () => {
           branchName="Tam Tac NVH"
         />
       </View>
+      <Calendar
+        current={currentDate}
+        markedDates={combinedMarkedDates}
+        onDayPress={markedSelectDate}
+      />
     </SafeAreaView>
   );
 };
+
 export default HomePage;
